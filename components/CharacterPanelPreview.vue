50814b664d7359ed5e722e614b52267e5c23a0ae
Molnar József(Pepó) <mojo12984@gmail.com>
2021-09-07 10:41:29 +0200

    some more potential components

---
+++ components/CharacterPanelPreview.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="$store.state.ujKarakter.karakterPicture">
+    <img :src="ujKarakter.karakterPicture" alt="crest" class="crest" />
+    {{ ujKarakter.karakterPicture }}
+  </div></template
+>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "CharacterPanelPreview",
+  computed: mapState(["ujKarakter"])
+};
+</script>
+
+<style scoped>
+.miniFlag {
+  width: 50px;
+  height: 50px;
+}
+.crest {
+  width: 200px;
+  height: 200px;
+}
+</style>
