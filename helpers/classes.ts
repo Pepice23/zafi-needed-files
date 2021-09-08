3491833adb7b588cd4b4711683617c2c685ff3c7
Pepice23 <mojo12984@gmail.com>
2021-09-08 08:58:01 +0200

    fixed and divided class helpers

---
+++ helpers/classes.ts
@@ -0,0 +1,69 @@
+export interface Classes {
+  className: string;
+  imgPath: string;
+}
+//Classes
+const warrior: Classes = {
+  className: 'Warrior',
+  imgPath: '/assets/class-icons/Warrior.png',
+};
+const hunter: Classes = {
+  className: 'Hunter',
+  imgPath: '/assets/class-icons/Hunter.png',
+};
+const deathKnight: Classes = {
+  className: 'Death Knight',
+  imgPath: '/assets/class-icons/Death_Knight.png',
+};
+const mage: Classes = {
+  className: 'Mage',
+  imgPath: '/assets/class-icons/Mage.png',
+};
+const priest: Classes = {
+  className: 'Priest',
+  imgPath: '/assets/class-icons/Priest.png',
+};
+const monk: Classes = {
+  className: 'Monk',
+  imgPath: '/assets/class-icons/Monk.png',
+};
+const rogue: Classes = {
+  className: 'Rogue',
+  imgPath: '/assets/class-icons/Rogue.png',
+};
+const warlock: Classes = {
+  className: 'Warlock',
+  imgPath: '/assets/class-icons/Warlock.png',
+};
+const shaman: Classes = {
+  className: 'Shaman',
+  imgPath: '/assets/class-icons/Shaman.png',
+};
+const paladin: Classes = {
+  className: 'Paladin',
+  imgPath: '/assets/class-icons/Paladin.png',
+};
+const druid: Classes = {
+  className: 'Druid',
+  imgPath: '/assets/class-icons/Druid.png',
+};
+const demonHunter: Classes = {
+  className: 'Demon Hunter',
+  imgPath: '/assets/class-icons/Demon_Hunter.png',
+};
+// END of Classes
+
+export const classes: Classes[] = [
+  warrior,
+  hunter,
+  deathKnight,
+  mage,
+  priest,
+  monk,
+  rogue,
+  warlock,
+  shaman,
+  paladin,
+  druid,
+  demonHunter,
+];
